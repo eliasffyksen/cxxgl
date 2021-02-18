f07b2b4703a555ef557b3ec358d499f1c007b712
Elias F. Fyksen <git@fyksen.net>
2021-02-18 18:38:14 +0000

    Change log.h

--- src/log.h
+++ src/log.h
@@ -3,11 +3,11 @@
 
 #include <iostream>
 
-#define ERR(x) std::cerr << ( x ) << std::endl
+#define ERR(x) std::cerr << x << std::endl
 #define FATAL_ERR(x) { ERR(x); exit(1); }
 
 #if DEBUG
-#define LOG(x) std::cout << ( x ) << std::endl
+#define LOG(x) std::cout << x << std::endl
 #else
 #define LOG(x)
 #endif
