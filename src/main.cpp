141c9009d9d57c4bba2a971e91adbfa9660178fb
Elias F. Fyksen <git@fyksen.net>
2021-02-22 00:12:45 +0000

    Add index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,26 @@ int main(void)
     LOG("Using OpenGL version: " << glGetString(GL_VERSION));
 
     float positions[] = {
-        -1.0f, -1.0f,
-        0.0f, 1.0f,
-        1.0f, -1.0f,
+        -0.5f,  0.5f, // TR
+         0.5f,  0.5f, // TL
+        -0.5f, -0.5f, // BR
+         0.5f, -0.5f, // BL
+    };
+
+    uint32_t indices[] = {
+        0, 1, 2,
+        1, 3, 2,
     };
 
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
@@ -56,7 +67,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
